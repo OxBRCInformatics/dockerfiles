12922e66ba48d0eaa62ace5b2ccf8b54619e3d8e
James Welch <james.welch@cs.ox.ac.uk>
2016-04-03 10:32:56 +0100

    Ch-ch-ch-ch changes Tested and now working

--- postfix/run.sh
+++ postfix/run.sh
@@ -7,7 +7,7 @@ echo Running...
 [ -z "${SMTP_PASSWORD}" ] && echo "SMTP_PASSWORD is not set" && exit 1
 
 #Get the domain from the server host name
-DOMAIN=`echo $SERVER_HOSTNAME |awk -F. '{$1="";OFS="." ; print $0}' | sed 's/^.//'`
+#DOMAIN=`echo $SERVER_HOSTNAME |awk -F. '{$1="";OFS="." ; print $0}' | sed 's/^.//'`
 
 #Comment default mydestination, we will set it bellow
 sed -i -e '/mydestination/ s/^#*/#/' /etc/postfix/main.cf
@@ -23,6 +23,8 @@ echo "smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd" >> /etc/postfix/m
 echo "smtp_sasl_security_options = noanonymous" >> /etc/postfix/main.cf
 
 echo "[$SMTP_SERVER]:587 $SMTP_USERNAME:$SMTP_PASSWORD" >> /etc/postfix/sasl_passwd
+mkdir /var/log/supervisor
+
 postmap /etc/postfix/sasl_passwd
 
-supervisord
+supervisord -n
